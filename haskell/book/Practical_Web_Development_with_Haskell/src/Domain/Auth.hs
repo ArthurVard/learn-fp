8d4acc9b7db1c3c40d8e29350352a23c0f54e435
Arthur Vardanyan <arthur@datasfer.com>
2019-01-21 21:17:12 +0400

    finish Auth implementation

--- haskell/book/Practical_Web_Development_with_Haskell/src/Domain/Auth.hs
+++ haskell/book/Practical_Web_Development_with_Haskell/src/Domain/Auth.hs
@@ -1,6 +1,35 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE QuasiQuotes      #-}
-module Domain.Auth where
+
+module Domain.Auth (
+  -- * Types
+  Auth(..),
+  Email,
+  mkEmail,
+  rawEmail,
+  Password,
+  mkPassword,
+  rawPassword,
+  UserId,
+  VerificationCode,
+  SessionId,
+  RegistrationError(..),
+  EmailVerificationError(..),
+  LoginError(..),
+
+  -- * Ports
+  AuthRepo(..),
+  EmailVerificationNotif(..),
+  SessionRepo(..),
+
+  -- * Use cases
+  register,
+  verifyEmail,
+  login,
+  resolveSessionId,
+  getUser
+) where
+
 
 import ClassyPrelude
 import Control.Monad.Except
@@ -16,13 +45,54 @@ data Auth = Auth
     , authPassword :: Password
     } deriving (Show, Eq)
 
-newtype Email = Email { emailRaw :: Text } deriving (Show, Eq)
+newtype Email = Email
+    { emailRaw :: Text
+    } deriving (Show, Eq)
+
+newtype Password = Password
+    { passwordRaw :: Text
+    } deriving (Show, Eq)
+
+type UserId = Int
+type SessionId = Text
+
+data LoginError = LoginErrorInvalidAuth
+                | LoginErrorEmailNotVerified
+                  deriving (Show, Eq)
+
+
+data RegistrationError  = RegistrationErrorEmailTaken
+                          deriving (Show, Eq)
+
+type VerificationCode = Text
+
+class Monad m => AuthRepo m where
+    addAuth :: Auth -> m (Either RegistrationError VerificationCode)
+    setEmailAsVerified :: VerificationCode -> m (Either EmailVerificationError ())
+    findUserByAuth :: Auth -> m (Maybe (UserId, Bool))
+    findEmailFromUserId :: UserId -> m (Maybe Email)
+
+class Monad m => EmailVerificationNotif m where
+    notifyEmailVerification :: Email -> VerificationCode -> m ()
+
+class Monad m => SessionRepo m where
+    newSession :: UserId -> m SessionId
+    findUserIdBySessionId :: SessionId -> m (Maybe UserId)
+
+
+data EmailVerificationError = EmailVerificationErrorInvalidCode
+                              deriving (Show, Eq)
+
+
+-------------------------------------------------------------------------------
+--
+
 rawEmail :: Email -> Text
 rawEmail = emailRaw
 
-newtype Password = Password { passwordRaw :: Text } deriving (Show, Eq)
 rawPassword :: Password -> Text
 rawPassword = passwordRaw
+
 
 -- >>> mkEmail "test"
 -- Left ["Not a valid email"]
@@ -52,17 +122,6 @@ mkPassword = validate Password
 -- Registration
 
 
-data RegistrationError  =
-    RegistrationErrorEmailTaken
-    deriving (Show, Eq)
-
-type VerificationCode = Text
-
-class Monad m => AuthRepo m where
-    addAuth :: Auth -> m (Either RegistrationError VerificationCode)
-
-class Monad m => EmailVerificationNotif m where
-    notifyEmailVerification :: Email -> VerificationCode -> m ()
 
 
 
@@ -85,6 +144,12 @@ instance EmailVerificationNotif IO where
         putStrLn $ "Notify " <> rawEmail email <> " - " <> vcode
 
 
+verifyEmail :: AuthRepo m => VerificationCode -> m (Either EmailVerificationError ())
+verifyEmail = setEmailAsVerified
+
+
+
+
 -- test
 ex03 :: IO (Either RegistrationError ())
 ex03 = do
@@ -92,3 +157,22 @@ ex03 = do
       Right password = mkPassword "1234ABCdef"
       auth = Auth email password
   register auth
+
+
+-------------------------------------------------------------------------------
+-- Login and Session
+login :: (AuthRepo m, SessionRepo m) => Auth -> m (Either LoginError SessionId)
+login auth = runExceptT $ do
+               result <- lift $ findUserByAuth auth
+               case result of
+                 Nothing         -> throwError LoginErrorInvalidAuth
+                 Just (_, False) -> throwError LoginErrorEmailNotVerified
+                 Just (uId, _)   -> lift $ newSession uId
+
+
+resolveSessionId :: SessionRepo m => SessionId -> m (Maybe UserId)
+resolveSessionId = findUserIdBySessionId
+
+
+getUser :: AuthRepo m => UserId -> m (Maybe Email)
+getUser = findEmailFromUserId
