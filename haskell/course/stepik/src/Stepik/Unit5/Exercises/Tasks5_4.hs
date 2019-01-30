6341c17637dac81f99068cf897e79705909ea645
Arthur Vardanyan <arthur@datasfer.com>
2019-01-30 23:52:44 +0400

    solve list monads effects task

--- haskell/course/stepik/src/Stepik/Unit5/Exercises/Tasks5_4.hs
+++ haskell/course/stepik/src/Stepik/Unit5/Exercises/Tasks5_4.hs
@@ -9,13 +9,12 @@ module Stepik.Unit5.Exercises.Tasks5_4 where
 -- data Board = ...
 -- Кроме того, пусть задана функция
 -- nextPositions :: Board -> [Board]
--- которая получает на вход некоторую конфигурацию доски и возвращает все возможные конфигурации, которые могут получиться, если какая-либо фигура сделает один ход. Напишите функцию:
+-- которая получает на вход некоторую конфигурацию доски и возвращает все возможные конфигурации,
+-- которые могут получиться, если какая-либо фигура сделает один ход. Напишите функцию:
 -- nextPositionsN :: Board -> Int -> (Board -> Bool) -> [Board]
--- которая принимает конфигурацию доски, число ходов n,
--- предикат и возвращает все возможные конфигурации досок,
--- которые могут получиться, если фигуры сделают n ходов и
--- которые удовлетворяют заданному предикату.
--- При n < 0 функция возвращает пустой список.
+-- которая принимает конфигурацию доски, число ходов n, предикат и возвращает все возможные
+-- конфигурации досок, которые могут получиться, если фигуры сделают n ходов и которые
+-- удовлетворяют заданному предикату. При n < 0 функция возвращает пустой список.
 
 -- dummy definitions
 data Board = Board Int deriving Show
@@ -23,10 +22,40 @@ data Board = Board Int deriving Show
 nextPositions :: Board -> [Board]
 nextPositions (Board i) = [Board (i + 1), Board (i + 2)]
 
--- solution
+-- solution A
 
 nextPositionsN :: Board -> Int -> (Board -> Bool) -> [Board]
-nextPositionsN  = undefined
+nextPositionsN b n pred =  nextPositionsNKleisli n pred b
+
+-- | filp args to make Kleisli
+nextPositionsNKleisli :: Int -> (Board -> Bool) ->  Board -> [Board]
+nextPositionsNKleisli n pred b
+          | n < 0 = []
+          | n == 0 = filter pred [b]
+          | otherwise = nextPositions b >>= nextPositionsNKleisli (n - 1) pred
+
+
+nextPositionsN' :: Board -> Int -> (Board -> Bool) -> [Board]
+nextPositionsN' b n pred
+  | n < 0 = []
+  | n == 0 = filter pred [b]
+  | otherwise = do
+    xs <- nextPositions b
+    ys <- nextPositionsN' xs (n - 1) pred
+    return ys
+
+{-
+-- | try convert recursive function on list tp list comprehension, not sure it is possible
+nextPositionsN'' b n pred = [ z | xs <- nextPositions b,
+                                   y <- xs,
+                                   z <- nextPositionsN'' y (n-1) pred, pred z]
+
+-}
+
+
+
+
+
 
 -------------------------------------------------------------------------------
 -- Exercise 5.4.3
