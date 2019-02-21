902f30723ed903ee4520d87c91d3e639d86016f9
Arthur Vardanyan <arthur@datasfer.com>
2019-02-21 23:02:13 +0400

    Start data61 course - Optional and List modules done.

---
+++ haskell/course/data61/fp-course/test/TastyLoader.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE ImplicitPrelude #-}
+
+import           Course.ApplicativeTest (test_Applicative)
+import           Course.ChequeTest      (test_Cheque)
+import           Course.ComonadTest     (test_Comonad)
+import           Course.ExtendTest      (test_Extend)
+import           Course.FunctorTest     (test_Functor)
+import           Course.JsonParserTest  (test_JsonParser)
+import           Course.ListTest        (test_List)
+import           Course.ListZipperTest  (test_ListZipper)
+import           Course.MonadTest       (test_Monad)
+import           Course.MoreParserTest  (test_MoreParser)
+import           Course.OptionalTest    (test_Optional)
+import           Course.ParserTest      (test_Parser)
+import           Course.StateTest       (test_State)
+import           Course.StateTTest      (test_StateT)
+import           Course.TraversableTest (test_Traversable)
+import           Course.ValidationTest  (test_Validation)
+import           Data.String            (fromString)
+import           Test.Tasty
+
+main :: IO ()
+main = defaultMain tests
+
+tests :: TestTree
+tests =
+  testGroup "Tests" [
+    test_Optional
+  , test_List
+--  , test_Functor
+--  , test_Applicative
+--  , test_Monad
+--  , test_MoreParser
+--  , test_Parser
+--  , test_State
+--  , test_StateT
+--  , test_Validation
+--  , test_Extend
+--  , test_Comonad
+--  , test_Traversable
+--  , test_ListZipper
+--  , test_JsonParser
+--  , test_Cheque
+  ]
